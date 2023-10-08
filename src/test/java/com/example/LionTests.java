886154c796a24c43dfb459aa1f29d8a3683912fb
Anastasiya Elkafrawi <atverdova78@gmail.com>
2023-10-08 19:03:23 +0400

    Проект спринт №6 Яндекс.Практикум

---
+++ src/test/java/com/example/LionTests.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import com.example.Feline;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LionTests {
+    @Mock
+    Feline feline;
+}
